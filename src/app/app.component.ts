6d3cf546f53e913025f4f2c0af8cdf70256983d4
Adam <adam.bialik85@gmail.com>
2021-12-17 11:23:02 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kronika';
+  
 }
+
+// Set-ExecutionPolicy -Scope Process Unrestricted
