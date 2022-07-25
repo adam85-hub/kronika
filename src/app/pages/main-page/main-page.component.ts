7c5a57c2f542f1f3e75854c713931b89f806a8b3
adam85-hub <56732487+adam85-hub@users.noreply.github.com>
2022-07-25 14:18:43 +0200

    Added history of site on main page on mobile

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { EntryModel } from 'src/app/models/entry.model';
 import { EntriesService } from 'src/app/services/entries.service';
@@ -7,10 +7,31 @@ import { PageComponent } from '../page/page.component';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent extends PageComponent implements OnInit {
   public newestEntries: EntryModel[] = [];
+  
+  articleOpened: boolean = false;
+
+  @ViewChild('articleText') articleTextEl?: ElementRef;
+
+  public get articleStyle() {
+    if (this.articleOpened) return {
+      "height": (this.articleTextEl?.nativeElement as HTMLParagraphElement).clientHeight + "px",
+      "padding": "10px"
+    };
+    else return {
+      "height": "0px",
+      "padding": "0px"
+    };
+  }
+
+  public get arrowStyle() {
+    return {
+      "transform": `rotate(${this.articleOpened ? "180deg" : "0deg"})`
+    };
+  }
 
   constructor(titleService: Title, private entriesService: EntriesService) { 
     super(titleService);
@@ -26,5 +47,10 @@ export class MainPageComponent extends PageComponent implements OnInit {
       response.forEach((entryInterface) => this.newestEntries.push(new EntryModel(entryInterface, undefined)));
       this.newestEntries.sort((a, b) => b.Date.valueOf() - a.Date.valueOf());
     });
+    
+  }
+
+  toggleHistoryMobile() {
+    this.articleOpened = !this.articleOpened;
   }
 }
