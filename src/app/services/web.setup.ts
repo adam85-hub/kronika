9ed4f7802feba76f404aee225c1c1e235fc1cc3f
adam85-hub <56732487+adam85-hub@users.noreply.github.com>
2022-12-15 15:16:47 +0100

    Added production web configuration

--- src/app/services/web.setup.ts
+++ src/app/services/web.setup.ts
@@ -1,3 +1,4 @@
+//* Development:
 const baseUrl = 'http://localhost:80';
 
 export const SETUP = {
@@ -5,3 +6,12 @@ export const SETUP = {
     apiUrl: baseUrl + "/kronika/api/public",
     entriesFolderUrl: baseUrl + "/kronika/entries/"
 }
+
+//* Production:
+// const baseUrl = 'http://fzskatowicepodlesie.pl';
+
+// export const SETUP = {
+//     baseUrl: baseUrl,
+//     apiUrl: baseUrl + "/parafia/api",
+//     entriesFolderUrl: baseUrl + "/parafia/entries/"
+// }
