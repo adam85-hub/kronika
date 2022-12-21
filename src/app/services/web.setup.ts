42f0e63dff88f0f5edd7ebdda5b2a3031b8f0b91
adam85-hub <56732487+adam85-hub@users.noreply.github.com>
2022-12-21 19:44:00 +0100

    Production config

--- src/app/services/web.setup.ts
+++ src/app/services/web.setup.ts
@@ -1,17 +1,17 @@
 //* Development:
-const baseUrl = 'http://localhost:80';
-
-export const SETUP = {
-    baseUrl: baseUrl,
-    apiUrl: baseUrl + "/kronika/api/public",
-    entriesFolderUrl: baseUrl + "/kronika/entries/"
-}
-
-//* Production:
-// const baseUrl = 'http://fzskatowicepodlesie.pl';
+// const baseUrl = 'http://localhost:80';
 
 // export const SETUP = {
 //     baseUrl: baseUrl,
-//     apiUrl: baseUrl + "/parafia/api",
-//     entriesFolderUrl: baseUrl + "/parafia/entries/"
+//     apiUrl: baseUrl + "/kronika/api/public",
+//     entriesFolderUrl: baseUrl + "/kronika/entries/"
 // }
+
+//* Production:
+const baseUrl = 'http://fzskatowicepodlesie.pl';
+
+export const SETUP = {
+    baseUrl: baseUrl,
+    apiUrl: baseUrl + "/parafia/api/public",
+    entriesFolderUrl: baseUrl + "/parafia/entries/"
+}
